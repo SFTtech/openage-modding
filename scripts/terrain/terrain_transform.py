e032e0875ceebb4913a54b692baefe839f9712a2
heinezen <heinezen@hotmail.de>
2018-03-14 20:56:48 +0100

    Add helpful comments.

--- scripts/terrain/terrain_transform.py
+++ scripts/terrain/terrain_transform.py
@@ -2,7 +2,16 @@
 
 # Copyright 2018-2018 the openage authors. See copying.md for legal info.
 
-import sys, argparse
+#
+# Pillow is required for image manipulation. Install with pip:
+#
+#     $ pip install pillow
+#
+# This script transforms an image from cartesian to dimetric projection. An
+# inverse operation is supported. Files for the Genie Engine can be created
+# by using a legacy mode that outputs BMP files.
+
+import argparse
 from PIL import Image
 
 parser = argparse.ArgumentParser(description='Transforms an image from cartesian to dimetric projection.')
@@ -26,11 +35,14 @@ def main():
 
     return 0
 
+# Flat to dimetric transformation
 def transform(org_img):
 
     res_x, res_y = org_img.size
 
+    # The transformed image is 2 times the size of the original
     if (legacy_mode):
+        # We need the background to be pink in legacy mode
         tr_img = Image.new( 'RGB', (2 * res_x, res_y), (255,0,255))
     else:
         tr_img = Image.new( 'RGBA', (2 * res_x, res_y), (0,0,0,0))
@@ -42,6 +54,15 @@ def transform(org_img):
     # Transform the image
     for x in range(0, res_x):
         for y in range(0, res_y):
+
+            # Every pixel is transformed with simple matrix
+            # multiplication. The projection matrix is
+            #
+            # | 1   -1   |
+            # | 0.5  0.5 |
+            #
+            # The x result has to be offset by the x value of
+            # the original image.
             c1 = (1 * x + res_x) - 1 * y
             c2 = 0.5 * x + 0.5 * y
 
@@ -51,6 +72,7 @@ def transform(org_img):
 
     return 0
 
+# Dimetric to flat transformation
 def inverse_transform(org_img):
 
     res_x, res_y = org_img.size
@@ -70,15 +92,19 @@ def inverse_transform(org_img):
     for x in range(0, tr_res_x):
         for y in range(0, res_y):
 
+            # This uses the exact calculation as in transform()
             c1 = (1 * x + tr_res_x) - 1 * y
             c2 = 0.5 * x + 0.5 * y
 
+            # We just need to swap (c1,c2) and (x,y) from the other
+            # function to revert the projection
             tr_pixels[x,y] = org_pixels[c1,c2]
 
     to_file(tr_img)
 
     return 0
 
+# Writes the transformed result to file
 def to_file(img):
 
     # Use this for debugging:
