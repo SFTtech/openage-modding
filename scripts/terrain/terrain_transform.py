b6e4eccfba5b3e93065bab674a812303fabfa89b
heinezen <heinezen@hotmail.de>
2018-03-17 13:19:28 +0100

    Use palette for legacy mode.

--- scripts/terrain/terrain_transform.py
+++ scripts/terrain/terrain_transform.py
@@ -21,43 +21,42 @@ def main():
     CLI entry point
     """
 
-    args = get_args()
+    args = getargs()
 
     inputfile = args.inputfile
     inverse = args.inverse
-    legacy_mode = args.legacy_mode
+    palette = args.palette_file
 
     org_img = Image.open(inputfile)
 
     if inverse:
-        tr_img = inverse_transform(org_img, legacy_mode)
+        tr_img = inverse_transform(org_img, palette)
     else:
-        tr_img = transform(org_img, legacy_mode)
+        tr_img = transform(org_img, palette)
 
     output_name = inputfile.split(".")[0] + "_t"
 
-    to_file(tr_img, legacy_mode, output_name)
+    to_file(tr_img, palette, output_name)
 
     return 0
 
-def get_args():
+def getargs():
     """
     Get CLI arguments.
     """
 
     parser = argparse.ArgumentParser(description=("Transforms an image from cartesian "
                                                   "to dimetric projection."))
-    parser.add_argument('inputfile')
+    parser.add_argument('inputfile', help='The image you want to transform')
     parser.add_argument('-i', '--inverse', default=False, action='store_true',
                         help='Transforms from dimetric to cartesian')
-    parser.add_argument('-l', '--legacy-mode', default=False, action='store_true',
-                        dest='legacy_mode',
+    parser.add_argument('--legacy-mode', dest='palette_file',
                         help=("Uses BMP instead of PNG as output format and the "
                               "color PINK (255,0,255) for background instead of the "
-                              "ALPHA channel"))
+                              "ALPHA channel. Requires an image with the AoE2 palette."))
     return parser.parse_args()
 
-def transform(img, legacy_mode):
+def transform(img, palette):
     """
     Flat to dimetric transformation.
     """
@@ -65,7 +64,7 @@ def transform(img, legacy_mode):
     res_x, res_y = img.size
 
     # The transformed image is 2 times the size of the original
-    if legacy_mode:
+    if palette:
         # We need the background to be pink in legacy mode
         tr_img = Image.new('RGB', (2 * res_x, res_y), (255, 0, 255))
     else:
@@ -95,10 +94,12 @@ def transform(img, legacy_mode):
 
             tr_pixels[tr_x, tr_y] = org_pixels[x_coord, y_coord]
 
+    if palette:
+        tr_img = tr_img.quantize(colors=256, palette=Image.open(palette))
 
     return tr_img
 
-def inverse_transform(img, legacy_mode):
+def inverse_transform(img, palette):
     """
     Dimetric to flat transformation.
     """
@@ -107,7 +108,7 @@ def inverse_transform(img, legacy_mode):
 
     tr_res_x = (int)((1/2) * res_x)
 
-    if legacy_mode:
+    if palette != None:
         tr_img = Image.new('RGB', (tr_res_x, res_y), (255, 0, 255))
     else:
         tr_img = Image.new('RGBA', (tr_res_x, res_y), (0, 0, 0, 0))
@@ -131,9 +132,12 @@ def inverse_transform(img, legacy_mode):
             # function to revert the projection
             tr_pixels[x_coord, y_coord] = org_pixels[tr_x, tr_y]
 
+    if palette != None:
+        tr_img = tr_img.quantize(colors=256, palette=Image.open(palette))
+
     return tr_img
 
-def to_file(img, legacy_mode, filename):
+def to_file(img, palette, filename):
     """
     Writes the transformed result to file.
     """
@@ -142,7 +146,7 @@ def to_file(img, legacy_mode, filename):
     # img.show()
 
     try:
-        if legacy_mode:
+        if palette:
             img.save(filename + ".bmp", "BMP")
         else:
             img.save(filename + ".png", "PNG")
